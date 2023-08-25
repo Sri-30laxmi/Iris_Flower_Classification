1cc6c767d880f8b737abb56b72634ffc46927ae0
Sri-30laxmi <140370008+Sri-30laxmi@users.noreply.github.com>
2023-08-25 12:50:19 +0530

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,32 @@
+# from flask import Flask, render_template, request
+# import pickle
+from flask import Flask, request, render_template
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib as joblib
+import os
+
+app = Flask(__name__)
+model = joblib.load('saved_model1.pkl')
+scaler = joblib.load('scaler.save')
+
+# app = Flask(__name__)
+# loading the model
+
+@app.route('/')
+def home():
+    result = ''
+    return render_template('index.html', **locals())
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    sepal_length = float(request.form['sepal_length'])
+    sepal_width = float(request.form['sepal_width'])
+    petal_length = float(request.form['petal_length'])
+    petal_width = float(request.form['petal_width'])
+    result  = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
+    return render_template('index.html', **locals())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
